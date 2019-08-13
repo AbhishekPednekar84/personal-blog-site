d65a34b9846160ad92992717bb4bb6e1e27e6548
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-08-13 22:22:20 +0530

    Changed SITEURL back to default

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Abhishek Pednekar'
 SITENAME = 'Code Disciples'
-SITEURL = 'codedisciples.in'
+SITEURL = ''
 SITESUBTITLE = u'A blog for all things code'
 
 PATH = 'content'
