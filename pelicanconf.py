c65866b4fbebd309d647d038f8503c5874f1e33f
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-08-13 22:11:51 +0530

    Changed the value of SITEURL in pelicanconf.py to codedisciples.in

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Abhishek Pednekar'
 SITENAME = 'Code Disciples'
-SITEURL = '/'
+SITEURL = 'codedisciples.in'
 SITESUBTITLE = u'A blog for all things code'
 
 PATH = 'content'
