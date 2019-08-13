eea9cf9e9f24ac78f6d423684258c393f54bb166
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-08-13 22:29:35 +0530

    SITEURL updated in publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append(os.curdir)
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://codedisciples.in'
 RELATIVE_URLS = False
 
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
