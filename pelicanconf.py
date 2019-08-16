3dae7d74efe82243aee17eccfcd88fd2de74566a
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-08-16 18:32:52 +0530

    Changed theme to voidy bootstrap :gem:

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Abhishek Pednekar'
 SITENAME = 'Code Disciples'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
 SITESUBTITLE = u'A blog for all things code'
 
 PATH = 'content'
@@ -14,40 +14,45 @@ TIMEZONE = 'Asia/Kolkata'
 DEFAULT_LANG = u'en'
 
 # Theme info - https://github.com/arulrajnet/attila
-THEME = "themes/attila"
+#THEME = "themes/attila"
+THEME = "themes/voidy-bootstrap"
 
 # Overriding CSS for the rss icon
-CSS_OVERRIDE = ['theme/css/blog.css']
-COLOR_SCHEME_CSS = 'monokai.css'
+# CSS_OVERRIDE = ['theme/css/blog.css']
+# COLOR_SCHEME_CSS = 'monokai.css'
+
+STYLESHEET_FILES = ("blog.css",)
+
+SIDEBAR = "sidebar.html"
 
 DEFAULT_PAGINATION = 7
 
 FAVICON = "static/images/favicon.ico"
 
 # Image for the home page
-HOME_COVER = "static/images/black-gradient-home.jpg"
+# HOME_COVER = "static/images/black-gradient-home.jpg"
 
 
 # Post and Pages path
-ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
-YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+# ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+# ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+# PAGE_URL = 'pages/{slug}/'
+# PAGE_SAVE_AS = 'pages/{slug}/index.html'
+# YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+# MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
 
-# Tags and Category path
-CATEGORY_URL = 'category/{slug}'
-CATEGORY_SAVE_AS = 'category/{slug}/index.html'
-CATEGORIES_SAVE_AS = 'catgegories.html'
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
-TAGS_SAVE_AS = 'tags.html'
+# # Tags and Category path
+# CATEGORY_URL = 'category/{slug}'
+# CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+# CATEGORIES_SAVE_AS = 'catgegories.html'
+# TAG_URL = 'tag/{slug}'
+# TAG_SAVE_AS = 'tag/{slug}/index.html'
+# TAGS_SAVE_AS = 'tags.html'
 
-# Author
-AUTHOR_URL = 'author/{slug}'
-AUTHOR_SAVE_AS = 'author/{slug}/index.html'
-AUTHORS_SAVE_AS = 'authors.html'
+# # Author
+# AUTHOR_URL = 'author/{slug}'
+# AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+# AUTHORS_SAVE_AS = 'authors.html'
 
 # Author bio
 AUTHORS_BIO = {
